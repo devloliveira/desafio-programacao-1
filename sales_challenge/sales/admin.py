482b2c64c9526e3affda2d9fdf6435403fb75d6d
Leonardo Oliveira <dev.loliveira@gmail.com>
2018-11-30 19:55:26 -0200

    Adiciona o modelo Transaction customizado para apresentacao dos dados de receita
    no painel do admin

--- sales_challenge/sales/admin.py
+++ sales_challenge/sales/admin.py
@@ -12,6 +12,23 @@ class ReadOnlyModelAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, *args, **kwargs):
         return False
+class TransactionChangeList(ChangeList):
+    def get_results(self, *args, **kwargs):
+        super().get_results(*args, **kwargs)
+
+        gross_revenue = 0
+        for transaction in self.result_list:
+            gross_revenue += transaction.product.price * transaction.purchase_count
+
+        self.gross_revenue = gross_revenue
+
+
+class TransactionModelAdmin(admin.ModelAdmin):
+    change_list_template = 'change_list.html'
+
+    def get_changelist(self, request):
+        return TransactionChangeList
 admin.site.register(Client, ReadOnlyModelAdmin)
 admin.site.register(Product, ReadOnlyModelAdmin)
 admin.site.register(Merchant, ReadOnlyModelAdmin)
+admin.site.register(Transaction, TransactionModelAdmin)
