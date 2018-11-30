1d9b21a2aeb57cf0a631fc1121e2d19a1f6bd181
Leonardo Oliveira <dev.loliveira@gmail.com>
2018-11-30 19:49:05 -0200

    Cria o modelo : Product

--- sales_challenge/sales/models.py
+++ sales_challenge/sales/models.py
@@ -14,3 +14,15 @@ class Client(BaseModel):
 
     def __str__(self):
         return '{name}'.format(name=self.name)
+
+
+class Product(BaseModel):
+    description = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    merchant = models.ForeignKey('Merchant', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.merchant:
+            return '{description}: R${price}, {merchant}'.format(description=self.description, price=self.price, merchant=self.merchant.name)
+        return '{description}: R${price} '.format(description=self.description, price=self.price)
+
