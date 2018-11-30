536d7c2ef693798118a4389eef3220a1be1784a5
Leonardo Oliveira <dev.loliveira@gmail.com>
2018-11-30 19:54:22 -0200

    Adiciona os modelos do projeto ao admin

--- sales_challenge/sales/admin.py
+++ sales_challenge/sales/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (
+    Client,
+    Product,
+    Merchant,
+    Transaction,
+    SalesFile
+)
 
-# Register your models here.
+
+class ReadOnlyModelAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, *args, **kwargs):
+        return False
+admin.site.register(Client, ReadOnlyModelAdmin)
+admin.site.register(Product, ReadOnlyModelAdmin)
+admin.site.register(Merchant, ReadOnlyModelAdmin)
